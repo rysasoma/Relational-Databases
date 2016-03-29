20bc149e2808cfbdff41a180e3a365666199ce8d
Sasikanth Kavuri <rysasoma@gmail.com>
2016-03-28 21:20:48 -0700

    Re-structured Tables and Views for Extra Credit Option

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,52 +5,35 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-DROP VIEW IF EXISTS OMW;
-DROP VIEW IF EXISTS Standings;
-DROP View IF EXISTS playermatches;
-DROP VIEW IF EXISTS Wins;
-DROP TABLE IF EXISTS Matches;
-DROP TABLE IF EXISTS Players;
+
+
+-- Tournaments Table
+CREATE TABLE IF NOT EXISTS Tournaments (
+	id SERIAL primary key,
+	name VARCHAR(255)
+);
 
 -- Players Table
-CREATE TABLE Players (
+CREATE TABLE IF NOT EXISTS Players (
 	id SERIAL primary key,
 	name varchar(255)
 );
 
 -- Matches Table
-CREATE TABLE Matches (
+CREATE TABLE IF NOT EXISTS Matches (
 	id SERIAL primary key,
-	player int references Players(id),
-	opponent int references Players(id),
-	result int
+	tournament INTEGER,
+	winner INTEGER references Players(id),
+	loser INTEGER references Players(id),
+	draw BOOLEAN 
 );
 
--- Wins View shows number of wins for each Player
-CREATE VIEW Wins AS
-	SELECT Players.id, COUNT(Matches.opponent) AS n 
-	FROM Players
-	LEFT JOIN (SELECT * FROM Matches WHERE result>0) as Matches
-	ON Players.id = Matches.player
-	GROUP BY Players.id;
 
--- Count View shows number of matches for each Player
-CREATE VIEW playermatches AS
-	SELECT Players.id, Count(Matches.opponent) AS n 
-	FROM Players
-	LEFT JOIN Matches
-	ON Players.id = Matches.player
-	GROUP BY Players.id;
-
--- Standings View shows number of wins and matches for each Player
-CREATE VIEW Standings AS 
-	SELECT Players.id,Players.name,Wins.n as wins, playermatches.n as matches 
-	FROM Players, playermatches, Wins
-	WHERE Players.id = Wins.id and Wins.id = playermatches.id;
-
--- OMW Standings shows number of "Opponent Match Wins" for each Player
-CREATE VIEW OMW AS
-	SELECT Players.id, SUM(Standings.wins) as opponentMatchWins
-	FROM Players, Matches, Standings
-	WHERE Players.id = Matches.player AND Matches.opponent = Standings.id
-	GROUP BY Players.id
+-- Scores Table
+CREATE TABLE IF NOT EXISTS Scores (
+	tournament INTEGER references Tournaments(id),
+	player INTEGER references Players(id),
+	score INTEGER,
+	matches INTEGER,
+	bye INTEGER	
+);
